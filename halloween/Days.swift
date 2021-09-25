8629fc66c27ebb8e0c6952fe8700c5c5f44bd313
Christopher Lee <clee95@u.rochester.edu>
2021-09-25 18:36:06 -0400

    created the model

---
+++ halloween/Days.swift
@@ -0,0 +1,25 @@
+//
+//  Days.swift
+//  halloween
+//
+//  Created by Lee, Christopher on 9/25/21.
+//
+
+import Foundation
+import UIKit
+
+class Days {
+    var daytime = true
+    let colors = [UIColor.brown, UIColor.black]
+    
+    
+    func getColor() -> UIColor {
+        if daytime == true {
+            return colors[0]
+        }
+        else{
+            return colors[1]
+        }
+    }
+    
+}
---
+++ halloween/day.swift
@@ -0,0 +1,9 @@
+//
+//  day.swift
+//  halloween
+//
+//  Created by Lee, Christopher on 9/25/21.
+//
+
+import Foundation
+
