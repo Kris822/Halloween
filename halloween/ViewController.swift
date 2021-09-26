4f9e3e44aee4e220f086613e91d19cc7f75c98e0
Christopher Lee <clee95@u.rochester.edu>
2021-09-26 12:40:20 -0400

    finished building the UI and added a tap gesture recognizer

--- halloween/ViewController.swift
+++ halloween/ViewController.swift
@@ -11,38 +11,54 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var appTitle: UILabel!
     
-    @IBOutlet weak var appButton: UIButton!
-    
+    @IBOutlet weak var appText: UILabel!
     @IBOutlet weak var candyImage: UIImageView!
     @IBOutlet weak var scarecrowImage: UIImageView!
     
     @IBOutlet weak var pumpkinImage: UIImageView!
     @IBOutlet weak var spooky: UIImageView!
+    @IBOutlet weak var skullImage: UIImageView!
+    
+    @IBOutlet weak var houseImage: UIImageView!
+    
+    @IBOutlet weak var candy2: UIImageView!
+    
+    @IBOutlet weak var candy3: UIImageView!
+    
+    
     var model = Days()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = model.getColor()
         appTitle?.text = NSLocalizedString("app_title", comment: "the app's title")
-        appButton?.setTitle(NSLocalizedString("button_text", comment: "the text on the button"), for: .normal)
-        appTitle.textColor = UIColor.orange
-        pumpkinImage.isHidden = model.isDaytimeImageVisible()
-        candyImage.isHidden = model.isDaytimeImageVisible()
-        spooky.isHidden = model.isNighttimeImageVisible()
-        scarecrowImage.isHidden = model.isNighttimeImageVisible()
-        
-    }
-
-    @IBAction func changeDayNight(_ sender: Any) {
-        model.switchDaytime()
-        self.view.backgroundColor = model.getColor()
+        appText?.text = NSLocalizedString("app_text", comment: "the app's text with instructions")
+        appTitle.textColor = model.getTextColor()
         appTitle.textColor = model.getTextColor()
         pumpkinImage.isHidden = model.isDaytimeImageVisible()
         candyImage.isHidden = model.isDaytimeImageVisible()
+        candy2.isHidden = model.isDaytimeImageVisible()
+        candy3.isHidden = model.isDaytimeImageVisible()
         spooky.isHidden = model.isNighttimeImageVisible()
         scarecrowImage.isHidden = model.isNighttimeImageVisible()
+        skullImage.isHidden = model.isNighttimeImageVisible()
+        houseImage.isHidden = model.isNighttimeImageVisible()
     }
-    
+
+    @IBAction func ontap(_ sender: Any) {
+        model.switchDaytime()
+        self.view.backgroundColor = model.getColor()
+        appTitle.textColor = model.getTextColor()
+        appText.textColor = model.getTextColor()
+        pumpkinImage.isHidden = model.isDaytimeImageVisible()
+        candyImage.isHidden = model.isDaytimeImageVisible()
+        candy2.isHidden = model.isDaytimeImageVisible()
+        candy3.isHidden = model.isDaytimeImageVisible()
+        spooky.isHidden = model.isNighttimeImageVisible()
+        scarecrowImage.isHidden = model.isNighttimeImageVisible()
+        skullImage.isHidden = model.isNighttimeImageVisible()
+        houseImage.isHidden = model.isNighttimeImageVisible()
+    }
 
 }
 
